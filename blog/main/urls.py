a744fbcd9aa786538f50c201002ef7b82f540f82
Salavat-VR <shashkevich41@gmail.com>
2021-04-07 22:17:07 +0300

    i hope last one

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home_page'),
-    path('about', views.about,  name='about_page'),
+    path('about', views.about, name='about_page'),
     path('post', views.post, name='posts_page'),
     path('post/create', views.post_create, name='post_create'),
     path('api/posts', views.post_api, name='api_posts'),
