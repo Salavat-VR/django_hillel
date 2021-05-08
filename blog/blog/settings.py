17b4d07674b8e34d8da9981774177f28534dc76f
Salavat-VR <shashkevich41@gmail.com>
2021-05-08 17:21:46 +0300

    added fill_posts command

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from celery.schedules import crontab
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
     'main',
     'debug_toolbar',
 ]
