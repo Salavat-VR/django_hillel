fe24d294df8626948b59bed79a09a42b55dcd4ba
Salavat-VR <shashkevich41@gmail.com>
2021-05-12 19:43:59 +0300

    added extension, cause it didnt work

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'blog',
     'main',
     'debug_toolbar',
