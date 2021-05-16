370d98fcce17c0a3c9d983523ef235a0e1380ae6
Salavat-VR <shashkevich41@gmail.com>
2021-05-16 19:45:26 +0300

    renamed: blog -> core

--- blog/core/celery.py
+++ blog/core/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('mysite')
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- blog/core/urls.py
+++ blog/core/urls.py
@@ -1,4 +1,4 @@
-"""blog URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('core/', include('core.urls'))
 """
 import debug_toolbar
 from django.contrib import admin
