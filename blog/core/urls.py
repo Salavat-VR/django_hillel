c9b78fa293009ed3a4981f1b86a47c783eaa7890
Dmytro <dmytro.shashkevych@ukr.net>
2021-07-13 10:23:27 -0600

    initial homework commit

--- blog/core/urls.py
+++ blog/core/urls.py
@@ -40,8 +40,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('account.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup', views.SignUpView.as_view(), name='sign_up'),
+
 
     re_path(r'^api/v1/swagger/(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r"^api/v1/swagger/$", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
